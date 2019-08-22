758f3649497b3ad5cea40fbce0d64c389484dd27
Pavani Pamarthy <pavani.pamarthy52@gmail.com>
2019-08-22 06:39:01 +0000

    Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,29 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
   def show
   end
 
   private
 
+  def require_authorized_for_current_lesson
+    if current_user.enrolled_in?(current_lesson.section.course)
+      
+    else
+      redirect_to course_path(current_course), alert: "Not enrolled"
+
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
+
 end
